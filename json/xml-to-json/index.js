2a3a632f19271eae09f5d935838249ab2cbbf14a
Tyler Tucker <webcooltz@duck.com>
2023-03-10 19:19:10 -0700

    Cleaning
    I cleaned up the files, file structure, and made the code variable names better. Changed the Loops to be in order: i, j, k, m

--- json/xml-to-json/index.js
+++ json/xml-to-json/index.js
@@ -1,147 +1,161 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const file = './output-json.json';
+const file = "./test-io/output-json.json";
 let xmlInput;
 let xmlType;
 
 // cleans the XML (takes out extra spaces, indents, etc.)
-function cleanXml(xml) {
-    const xmlString = xml.replace(/\r?\n|\r| /g, "");
-
-    if (typeof xmlString === "Object") {
-        const tagSearch = xmlString.match(/<\?.*\?>/g);
-        const cleanXml = xmlString.split(tagSearch).splice(1, 1).join('');
-        return cleanXml;
-    } else {
-        return xmlString;
-    }
+function cleanupXml(xml) {
+  const cleanXmlString = xml.replace(/\r?\n|\r| /g, "");
+  return cleanXmlString;
 }
 
 // turns the XML into an array of strings (based on same tags)
-function xmlArrayCreater(xml) {
-    const tagSearch = xml.match(/<\w*\>/g);
-    const objectName = tagSearch[0];
-    const objectNameCount = (xml.match(new RegExp(objectName, 'gi')));
-    if (objectNameCount.length > 1) {
-        xmlType = 'Array';
+function changeXmlToArray(xml) {
+  const tags = xml.match(/<\w*\>/g);
+  const mainObjectTag = tags[0];
+  const objectTagCount = xml.match(new RegExp(mainObjectTag, "gi"));
+  if (objectTagCount.length > 1) {
+    xmlType = "Array";
 
-        let objectSplit = xml.split(objectName);
-        objectSplit.splice(0, 1);
+    let splitByObjectTag = xml.split(mainObjectTag);
+    splitByObjectTag.splice(0, 1);
 
-        for (var m = 0; m < objectSplit.length; m++) {
-            objectSplit[m] = objectName + objectSplit[m];
-        }
-        return objectSplit;
-    } else if (objectNameCount.length < 2) {
-        xmlType = 'String';
-        let stringToArray = [];
-        stringToArray.push(xml);
-        return stringToArray;
-    } else {
-        xmlType = 'Unknown';
-        console.log('error');
-        return;
+    for (var i = 0; i < splitByObjectTag.length; i++) {
+      splitByObjectTag[i] = mainObjectTag + splitByObjectTag[i];
     }
+    return splitByObjectTag;
+  } else if (objectTagCount.length < 2) {
+    xmlType = "String";
+    const stringToArray = [xml];
+    return stringToArray;
+  } else {
+    xmlType = "Unknown";
+    console.log("error");
+    return;
+  }
 }
 
 // cuts off the angled brackets in the tags
-function tagCutter(tagString) {
-    if (!tagString) {
-        return;
-    } else if (tagString.includes('</')) {
-        let tagCutterBeg = tagString.split('>');
-        let tagCutterEnd = tagCutterBeg[1].split('</');
-        return tagCutterEnd[0];
-    } else {
-        let tagCutterBeg = tagString.split('<');
-        let tagCutterEnd = tagCutterBeg[1].split('>');
-        return tagCutterEnd[0];
-    }
+function cutTags(tagString) {
+  let tagCutterBeginning;
+  let tagCutterEnd;
+  if (!tagString) {
+    return;
+  } else if (tagString.includes("</")) {
+    tagCutterBeginning = tagString.split(">");
+    tagCutterEnd = tagCutterBeginning[1].split("</");
+    return tagCutterEnd[0];
+  } else {
+    tagCutterBeginning = tagString.split("<");
+    tagCutterEnd = tagCutterBeginning[1].split(">");
+    return tagCutterEnd[0];
+  }
 }
 
 // main function
 function parseXml(xml) {
-    if (!xml) {
-        console.log(err);
-    } else {
-        let finalResult = [];
-        let finalResultString;
+  if (!xml) {
+    console.log("generic error");
+  } else {
+    let finalResult = [];
+    let finalResultString;
 
-        for (var i = 0; i < xml.length; i++) {
-            // finds all properties within the tags
-            let propertySearch = xml[i].match(/>\w*\<\//g);
+    for (var j = 0; j < xml.length; j++) {
+      // finds all properties within the tags
+      const propertyValues = xml[j].match(/>\w*\<\//g);
 
-            // finds all XML tags by '<' and '>'
-            let tagSearch = xml[i].match(/<\w*\>/g);
+      // finds all XML tags by '<' and '>'
+      const openingTags = xml[j].match(/<\w*\>/g);
 
-            // finds object name
-            let objectName = tagSearch[0];
-            let objectNameCut = tagCutter(objectName);
+      // finds object name
+      const mainObjectTag = openingTags[0];
+      const objectNameAfterCut = cutTags(mainObjectTag);
 
-            let propList = [];
-            let tagList = [];
+      let propertyList = [];
+      let tagList = [];
 
-            // makes property tags into JSON and pushes them to an array
-            for (var j = 0; j < propertySearch.length - 1; j++) {
-                let jsonProperty = propertySearch[j];
-                let propCut = tagCutter(jsonProperty);
+      // makes property tags into JSON and pushes them to an array
+      for (var k = 0; k < propertyValues.length - 1; k++) {
+        const jsonProperty = propertyValues[k];
+        const propertyAfterCut = cutTags(jsonProperty);
 
-                propList.push(propCut);
-            }
-            
-            // makes property tags into JSON format and pushes them to an array
-            for (var k = 1; k < tagSearch.length; k++) {
-                let jsonTag = tagSearch[k];
-                let tagCut = tagCutter(jsonTag);
+        propertyList.push(propertyAfterCut);
+      }
 
-                if (k === tagSearch.length - 1) {
-                    tagList.push('"' + tagCut + '"' + ': ' + '"' + propList[k-1]+ '"');
-                } else {
-                    tagList.push('"' + tagCut + '"' + ': ' + '"' + propList[k-1]+ '"' + ', ');
-                }
-            }
+      // makes property tags into JSON format and pushes them to an array
+      for (var m = 1; m < openingTags.length; m++) {
+        const jsonTag = openingTags[m];
+        const tagsAfterCut = cutTags(jsonTag);
 
-            // joins the array of tags
-            let tagSquish = tagList.join('');
-            // adds the quotes and brackets for JSON objects
-            let jsonResult = '{ ' + '"' + objectNameCut + '"' + ': ' + ' { ' + tagSquish + ' } }';
-
-            // console.log('jsonResult: ', jsonResult);
-
-            if (i === xml.length - 1) {
-                finalResult.push(jsonResult);
-            } else {
-                finalResult.push(jsonResult + ',');
-            }
-        }
-
-        // turns it into an array in JSON
-        if (xmlType === "Array") {
-            finalResultString = '[' + finalResult.join('') + ']';
+        if (m === openingTags.length - 1) {
+          tagList.push(
+            '"' + tagsAfterCut + '"' + ": " + '"' + propertyList[m - 1] + '"'
+          );
         } else {
-            finalResultString = finalResult.join('');
+          tagList.push(
+            '"' +
+              tagsAfterCut +
+              '"' +
+              ": " +
+              '"' +
+              propertyList[m - 1] +
+              '"' +
+              ", "
+          );
         }
+      }
 
-        console.log('finalResult: ', finalResult);
+      // joins the array of tags
+      const squishedTags = tagList.join("");
 
-        fs.writeFile(file, finalResultString, (err) => {
-            if (err) throw err;
-            console.log('Data has been written to file: ', file);
-        });
+      // adds the quotes & brackets for JSON objects
+      const jsonResult =
+        "{ " +
+        '"' +
+        objectNameAfterCut +
+        '"' +
+        ": " +
+        " { " +
+        squishedTags +
+        " } }";
+
+      if (j === xml.length - 1) {
+        finalResult.push(jsonResult);
+      } else {
+        finalResult.push(jsonResult + ",");
+      }
     }
+
+    // turns it into an array in JSON
+    if (xmlType === "Array") {
+      finalResultString = "[" + finalResult.join("") + "]";
+    } else {
+      finalResultString = finalResult.join("");
+    }
+
+    console.log("Final Result: ", finalResult);
+    // console.log("Final Result: ", finalResultString);
+
+    fs.writeFile(file, finalResultString, (err) => {
+      if (err) throw err;
+      console.log("Data has been written to file: ", file);
+    });
+  }
 }
 
 // input-xml-singular
 // input-xml-messy
-fs.readFile('./input-xml-singular.xml', 'utf8', (err, xmlData) => {
-    if (err) {
-      return console.log(err);
-    } else {
-        xmlInput = xmlData;
-        const cleanString = cleanXml(xmlInput);
-        const arrayString = xmlArrayCreater(cleanString);
-        parseXml(arrayString);
-    }
+// input-xml
+fs.readFile("./test-io/input-xml-messy.xml", "utf8", (err, xmlData) => {
+  if (err) {
+    return console.log(err);
+  } else {
+    xmlInput = xmlData;
+    const cleanInput = cleanupXml(xmlInput);
+    const arrayInput = changeXmlToArray(cleanInput);
+    parseXml(arrayInput);
+  }
 });
 
 // module.exports = parseXml(xmlInput);
